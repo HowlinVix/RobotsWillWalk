68f0679a190f01e93cb36ac49416fafef2179242
SterlingVix <amelocik@gmail.com>
2014-10-21 15:52:12 -0700

    first commit with HTML boilerplate built.

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// Find the DOM element with the class name "button1"
+var button = document.getElementsByClassName("button1");
+var instructionsDiv = document.getElementsByClassName("instructions");
+
+// getElementsByClassName returns an array: [<button1>]
+button = button[0];
+    console.log("test1");
+// button[0].on()
+// button.on('click', clickResult);
+
+
+// ERROR
+// Button does not have a method "on."
+// We need to use an event handling function...
+
+
+button.on('click', function() {
+    
+});
+
+var clickResult = function() {
+    // instructionsDiv has the properties:
+        // instructionsDiv.class = "instructions"
+        // instructionsDiv.style = "border: solid orange 2px; font-family: fantasy"
+    instructionsDiv.stlye = "border: solid red 2px;";
+    console.log("test1");
+    
+}; // end clickResult()
