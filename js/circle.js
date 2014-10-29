0ae22d3d8b135f18c45001be3189a594d137c289
SterlingVix <amelocik@gmail.com>
2014-10-29 15:12:34 -0700

    Objects animate.

---
+++ js/circle.js
@@ -0,0 +1,23 @@
+var circlesvg = document.getElementsByClassName("circlesvg")[0];
+var circle1 = document.getElementsByClassName("circle1")[0];
+
+var circle = {
+  'x' : 125, // circle.x
+  'y' : 125, // circle.y
+  'r' : 25 // Redundant property
+};
+
+// THIS CONTENT WILL CHANGE, so it needs to be calculated each time.
+// That means it needs to be in a function...
+  //circle.right = circle.x + circle.r;
+  //circle.left = circle.x + circle.r;
+
+var circle2 = {};
+circle2.x = 100;
+circle2.y = 100;
+circle2.width = 50;
+circle2.height = 50;
+
+//var ellipseX = 125;
+//var ellipseY = 125;
+
---
+++ js/heroBot.js
@@ -0,0 +1,16 @@
+/*********
+ * heroBot should:
+ * 1) march LEFT TO RIGHT
+ * 2) disappear when it goes off screen
+ * 3) BONUS - new hero bot should appear each
+ *    time you click a button.
+ ********/
+ 
+
+
+// TODO: march LEFT TO RIGHT
+
+
+
+
+// TODO: disappear when it goes off screen
